bf6283384e19ce9e117158e3fdf8574e0811f4f4
Almas fiza <139757263+Almas-fiza@users.noreply.github.com>
2023-10-02 23:28:23 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 "use strict";
-//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
-//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
-//Paulo coelho said “A life without a cause is a life without effect.”
-console.log('Paulo coelho said “A life without a cause is a life without effect."');
-//Q5 Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your messag
-let famous_person = "Paulo coelho";
-let message = famous_person + ' once said , “A life without a cause is a life without effect."';
-console.log(message);
+//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+let Name = "\t  Almas Fiza \n";
+console.log("Name with whitespace:", Name);
+let strippedName = Name.trim();
+console.log("stripped name:", strippedName);
