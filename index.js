a052b38d97e8594c0feca6d3b6269e56c4dffe31
Almas fiza <139757263+Almas-fiza@users.noreply.github.com>
2023-10-02 23:40:34 +0500

    q10

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//Almas Fiza
+// this program calculates sum of two numbers and prints the result.
+let num1 = 5;
+let num2 = 10;
+let sum = num1 + num2;
+console.log("the sum is: " + sum);
---
+++ index.ts
@@ -0,0 +1,9 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+//Almas Fiza
+// this program calculates sum of two numbers and prints the result.
+
+let num1: number = 5;
+let num2 : number = 10;
+let sum : number = num1 + num2 ;
+console.log("the sum is: " + sum);
