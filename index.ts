7c6031ff25bfbed2d974aed79f484efc745d33bc
Almas fiza <139757263+Almas-fiza@users.noreply.github.com>
2023-10-02 23:26:35 +0500

    q4 & 5

---
+++ index.ts
@@ -0,0 +1,13 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+//Paulo coelho said “A life without a cause is a life without effect.”
+
+console.log('Paulo coelho said “A life without a cause is a life without effect."')
+
+//Q5 Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your messag
+
+let famous_person : string ="Paulo coelho";
+let message : string = famous_person + ' once said , “A life without a cause is a life without effect."'
+console.log(message);
