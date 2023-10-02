47f09563b0de09a01c1607961492e1abf4386596
Almas fiza <139757263+Almas-fiza@users.noreply.github.com>
2023-10-02 23:20:17 +0500

    q6

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+let Name = "\t  Almas Fiza \n";
+console.log("Name with whitespace:", Name);
+let strippedName = Name.trim();
+console.log("stripped name:", strippedName);
